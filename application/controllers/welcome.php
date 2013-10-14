c90f5abb2ab74c3eca2cd0c378632ea0a78691e1
Pulipuli Chen <pulipuli.chen@gmail.com>
2013-10-14 21:04:38 +0800

    載入jQuery有點問題

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -19,7 +19,9 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+            $this->load->library('javascript');
+            $this->load->library('jquery');
+            $this->load->view('upload_view');
 	}
 }
 
